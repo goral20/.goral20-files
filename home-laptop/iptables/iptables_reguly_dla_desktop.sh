044d0072ff160954ce13d01a408db335fcdbcc25
goral20 <goral20@gmail.com>
2013-03-17 16:43:58 +0100

    iptables files

--- home-laptop/iptables/iptables_reguly_dla_desktop.sh
+++ home-laptop/iptables/iptables_reguly_dla_desktop.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Dodaje Reguły dla IPtables"
 
 # CZYSZCZENIE STARYCH REGUŁ
 iptables -F
@@ -20,7 +21,6 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A FORWARD -o lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
-
 # NAWIĄZANIE POŁĄCZENIA
 iptables -A INPUT -j ACCEPT -m state --state ESTABLISHED,RELATED
 iptables -A FORWARD -j ACCEPT -m state --state ESTABLISHED,RELATED
@@ -28,12 +28,12 @@ iptables -A FORWARD -j ACCEPT -m state --state ESTABLISHED,RELATED
 ## Regułki
 # FTP
 # FTP tryb aktywny
-iptables -A OUTPUT -p tcp --dport 20 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 21 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
 
 # FTP tryb pasywny
-iptables -A OUTPUT -p tcp --dport 1024 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 1025 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 1024:1025 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 1024:1025 -j ACCEPT
 
 # SSH
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
@@ -56,10 +56,7 @@ iptables -A OUTPUT -p tcp --dport 138 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 139 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 445 -j ACCEPT
 
-
-
-
-## Stare
+## Dla serwera 
 # SSH
 #/sbin/iptables -A INPUT -s 0/0 -p tcp --dport 10025 -j ACCEPT
 #/sbin/iptables -A INPUT -s 0/0 -p udp --dport 10025 -j ACCEPT
@@ -85,37 +82,20 @@ iptables -A OUTPUT -p tcp --dport 445 -j ACCEPT
 # odblokowanie uPnP
 #/sbin/iptables -A INPUT -i eth0 -p udp --dport 1900 -j ACCEPT
 #/sbin/iptables -A OUTPUT -o eth0 -p udp --sport 1900 -j ACCEPT
+## KONIEC
 
-#PING
+# PING (dopuszczenie)
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
+
+# PING (zablokowanie)
+# iptables -A INPUT -p icmp --icmp-type echo-request -j REJECT --reject-with icmp-host-unreachable
 
 # Blokowanie Telnetu
 iptables -A OUTPUT -p tcp --dport telnet -j REJECT
 iptables -A INPUT -p tcp --dport telnet -j REJECT
 
-# ochrona przed atakiem typu Smurf (stare)
-# echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
-
-# nie akceptujemy pakietow "source route" (zmieniaja tablice routingu) (stare)
-# echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
-
-# nie przyjmujemy pakietow ICMP redirect, ktore moga zmienic nasza tablice routingu (stare)
-# echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
-
-# wlaczamy ochrone przed blednymi pakietami ICMP error (stare)
-# echo 1 > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
-
-# ochrona przed spoofingiem -kazdy interfejs sieciowy bedzie przyjmowal (stare)
-# tylko te pakiety ktore znajduja sie w tablicy routingu
-# echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
-
-# wlacza logowanie dziwnych (spoofed, source routed, redirects) pakietow (stare)
-# echo 1 > /proc/sys/net/ipv4/conf/all/log_martians
-
-# Blokada przed atakami typu SYN FLOODING (stare)
-# echo 1 > /proc/sys/net/ipv4/tcp_syncookies
-
+# Ochrona przed skanowaniem
 # nmap -sS /skanowanie portów
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH SYN -j LOG --log-prefix "SKANOWANIE SYN_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH SYN -j DROP
@@ -125,18 +105,18 @@ iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH ACK -j DROP
 
 # nmap -sF (Fin)
-iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN -j LOG --log-prefix "SKANOWANIE Fin_"
+iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN -j LOG --log-prefix "SKANOWANIE FIN_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN -j DROP
 
 # nmap -sX / Xmas Tree
-iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN,URG,PSH -j LOG --log-prefix "SKANOWANIE Xmas_"
+iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN,URG,PSH -j LOG --log-prefix "SKANOWANIE XMAS_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN,URG,PSH -j DROP
 
 # nmap -sN / Null scan
 iptables -A INPUT -m conntrack --ctstate INVALID -p tcp ! --tcp-flags SYN,RST,ACK,FIN,PSH,URG SYN,RST,ACK,FIN,PSH,URG -j LOG --log-prefix "Null scan: "
 iptables -A INPUT -m conntrack --ctstate INVALID -p tcp ! --tcp-flags SYN,RST,ACK,FIN,PSH,URG SYN,RST,ACK,FIN,PSH,URG -j DROP # Skanowanie Null (nmap -sN)
 
-# ochrona przed atakami
+# Ochrona przed atakami
 # Ping of death
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j LOG --log-prefix "Ping: "
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT # Ping of death
@@ -156,31 +136,40 @@ iptables -A INPUT -p tcp --syn -m limit --limit 1/s -j ACCEPT
 # Furtive port scanner
 iptables -A INPUT -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
 
+# Blokowanie www
+iptables -A OUTPUT -p tcp -m string --string "onet.pl" --algo kmp -j DROP
+
 # SYN flood protection
 echo "1" > /proc/sys/net/ipv4/tcp_syncookies
+# echo 1 > /proc/sys/net/ipv4/tcp_syncookies
 
 # Wyłączamy odpowiedzi na pingi
 echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_all
 
 # Ochrona przed atakami typu Smurf
-echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts:
+echo "1" > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+# echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 
-# Włączamy ochronę przed komunikacją ICMP error
+# Włączamy ochronę przed komunikacją ICMP error (błędne pakiety ICMP)
 echo "1" > /proc/sys/net/ipv4/icmp_ignore_bogus_error_responses
 
 # Włącza logowanie dźiwnych pakietów (spoofed. source routed. redirects):
 echo "0" > /proc/sys/net/ipv4/conf/all/log_martians
+# echo 1 > /proc/sys/net/ipv4/conf/all/log_martians
 
-# Nie akceptujemy datagramu IP z opcją “source route”
+# Nie akceptujemy datagramu IP z opcją “source route” // nie akceptujemy pakietow "source route" (zmieniaja tablice routingu)
 echo "0" > /proc/sys/net/ipv4/conf/all/accept_source_route
+# echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route
 
 # Nie przyjmujemy pakietów ICMP redict, które mogą zmienić naszą tablicę routingu:
 echo "0" /proc/sys/net/ipv4/conf/all/accept_redirects
+# echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects
 
 # Wszystkie karty nie bedą przyjmowały pakietów z sieci innych niż te z tablicy routingu
 echo "0" /proc/sys/net/ipv4/conf/all/rp_filter
 
-# Blokowanie www
-iptables -A OUTPUT -p tcp -m string --string "onet.pl" --algo kmp -j DROP
+# ochrona przed spoofingiem -kazdy interfejs sieciowy bedzie przyjmowal (stare)
+# tylko te pakiety ktore znajduja sie w tablicy routingu
+# echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter
 
 echo "Reguły dla IPtables zostały załadowane"
