cb6413b6c353e08335339dced0e53ce403354e52
goral20 <goral20@gmail.com>
2013-03-18 22:44:08 +0100

    conky files

--- home-laptop/conky/.conky/okładka/sonata.sh
+++ home-laptop/conky/.conky/okładka/sonata.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-sonata info >/home/sebastian/.conky/okładka/textsonata
-if [ -f /home/sebastian/.conky/okładka/textsonata ]; then
-	YEAR=$(cat /home/sebastian/.conky/okładka/textsonata | grep Data | cut -c 7-)
-	ALBUM=$(cat /home/sebastian/.conky/okładka/textsonata | grep Album | cut -c 8-)
-	echo "$ALBUM - $YEAR.jpg" > /home/sebastian/.conky/okładka/text1
-	IMAGE=$(cat /home/sebastian/.conky/okładka/text1)
+sonata info >/home/sebastian/Dokumenty/Conky/okładka/textsonata
+if [ -f /home/sebastian/Dokumenty/Conky/okładka/textsonata ]; then
+	YEAR=$(cat /home/sebastian/Dokumenty/Conky/okładka/textsonata | grep Data | cut -c 7-)
+	ALBUM=$(cat /home/sebastian/Dokumenty/Conky/okładka/textsonata | grep Album | cut -c 8-)
+	echo "$ALBUM - $YEAR.jpg" > /home/sebastian/Dokumenty/Conky/okładka/text1
+	IMAGE=$(cat /home/sebastian/Dokumenty/Conky/okładka/text1)
 	if [ -f "/home/sebastian/Muzyka/.covers/$IMAGE" ]; then
-		cp "/home/sebastian/Muzyka/.covers/$IMAGE" "/home/sebastian/.conky/okładka/currentcover.jpg"
+		cp "/home/sebastian/Muzyka/.covers/$IMAGE" "/home/sebastian/Dokumenty/Conky/okładka/currentcover.jpg"
 	else
 		if [ -f "/home/sebastian/Muzyka/.covers/$ALBUM - $YEAR.jpg" ]; then
-			cp "/home/sebastian/Muzyka/.covers/$ALBUM - $YEAR.jpg" "/home/sebastian/.conky/okładka/currentcover.jpg"
+			cp "/home/sebastian/Muzyka/.covers/$ALBUM - $YEAR.jpg" "/home/sebastian/Dokumenty/Conky/okładka/currentcover.jpg"
 		else
-			cp "/home/sebastian/.conky/okładka/nocover.jpg" "/home/sebastian/.conky/okładka/currentcover.jpg"
+			cp "/home/sebastian/Dokumenty/Conky/okładka/nocover.jpg" "/home/sebastian/Dokumenty/Conky/okładka/currentcover.jpg"
 		fi
 	fi
 else
