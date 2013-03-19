520a9e8fdbf37901d19f186582e11369409c378b
goral20 <goral20@gmail.com>
2013-03-19 07:48:41 +0100

    iptables files

--- home-laptop/iptables/iptables_reguly_dla_desktop.sh
+++ home-laptop/iptables/iptables_reguly_dla_desktop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Dodaje Reguły dla IPtables"
-echo "Czyszczenie starych reguł"
 
+echo "Czyszczenie starych reguł"
 # CZYSZCZENIE STARYCH REGUŁ
 iptables -F
 iptables -X
@@ -10,6 +10,7 @@ iptables -X -t nat
 iptables -F -t filter
 iptables -X -t filter
 
+echo "Ustawienie polityki działania" 
 # USTAWIENIE POLITYKI DZIAŁANIA
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
@@ -18,16 +19,20 @@ iptables -P OUTPUT DROP
 #iptables -A OUTPUT -m state -p icmp --state INVALID -j DROP
 ##iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+echo "Dopuszczenie localhostu"
 # Odblokowanie / Dopuszczenie localhostu
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A FORWARD -o lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
+echo "Nawiązanie połączenia"
 # NAWIĄZANIE POŁĄCZENIA
 iptables -A INPUT -j ACCEPT -m state --state ESTABLISHED,RELATED
 iptables -A FORWARD -j ACCEPT -m state --state ESTABLISHED,RELATED
 
+echo "Dodanie regułek"
 ## Regułki
+echo "Reguła FTP"
 # FTP
 # FTP tryb aktywny
 iptables -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
@@ -37,22 +42,27 @@ iptables -A OUTPUT -p tcp --dport 20:21 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 1024:65535 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 1024:65535 -j ACCEPT
 
+echo "Reguła SSH"
 # SSH
 iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 10025 -j ACCEPT
 
+echo "Regula DNS"
 # DNS
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 #iptables -A INPUT -p udp --sport 53 -j ACCEPT
 
+echo "Reguła WWW"
 # WWW
 iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
+echo "Reguła Poczta"
 # Poczta
 iptables -A OUTPUT -p tcp --dport 143 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 993 -j ACCEPT
 
+echo "Reguła Samba"
 # Samba
 iptables -A OUTPUT -p tcp --dport 137 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 138 -j ACCEPT
@@ -87,6 +97,7 @@ iptables -A OUTPUT -p tcp --dport 445 -j ACCEPT
 #/sbin/iptables -A OUTPUT -o eth0 -p udp --sport 1900 -j ACCEPT
 ## KONIEC
 
+echo "Dopuszczenie PING"
 # PING (dopuszczenie)
 iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
@@ -94,38 +105,50 @@ iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 # PING (zablokowanie)
 # iptables -A INPUT -p icmp --icmp-type echo-request -j REJECT --reject-with icmp-host-unreachable
 
+echo "Blokowanie Telnetu"
 # Blokowanie Telnetu
 iptables -A OUTPUT -p tcp --dport telnet -j REJECT
 iptables -A INPUT -p tcp --dport telnet -j REJECT
 
+echo "Ochrona przed atakami"
 # Ochrona przed skanowaniem
+
+echo "Ochrona przez skanowaniem SYN"
 # nmap -sS /skanowanie portów
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH SYN -j LOG --log-prefix "SKANOWANIE SYN_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH SYN -j DROP
 
+echo "Ochrona przez skanowaniem ACK"
 # nmap -sA /filtr pakietów (ACK)
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH ACK -j LOG --log-prefix "SKANOWANIE FILTR_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH ACK -j DROP
 
+echo "Ochrona przez skanowaniem FIN"
 # nmap -sF (Fin)
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN -j LOG --log-prefix "SKANOWANIE FIN_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN -j DROP
 
+echo "Ochrona przez skanowaniem XMAS"
 # nmap -sX / Xmas Tree
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN,URG,PSH -j LOG --log-prefix "SKANOWANIE XMAS_"
 iptables -A INPUT -m conntrack --ctstate NEW -p tcp --tcp-flags SYN,RST,ACK,FIN,URG,PSH FIN,URG,PSH -j DROP
 
+echo "Ochrona przez skanowanie NULL"
 # nmap -sN / Null scan
 iptables -A INPUT -m conntrack --ctstate INVALID -p tcp ! --tcp-flags SYN,RST,ACK,FIN,PSH,URG SYN,RST,ACK,FIN,PSH,URG -j LOG --log-prefix "Null scan: "
 iptables -A INPUT -m conntrack --ctstate INVALID -p tcp ! --tcp-flags SYN,RST,ACK,FIN,PSH,URG SYN,RST,ACK,FIN,PSH,URG -j DROP # Skanowanie Null (nmap -sN)
 
+echo "Ochrona przez atakami"
 # Ochrona przed atakami
+
+echo "Ochrona przed atakiem PING OF DEATH"
 # Ping of death
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j LOG --log-prefix "Ping: "
 iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s -j ACCEPT # Ping of death
 # Zablokowanie Pingowania
 # iptables -A INPUT -p icmp --icmp-type echo-request -j REJECT --reject-with icmp-host-unreachable
 
+echo "Ochrona przed atakiem DOS"
 # Łańcuch syn-flood (obrona przed DoS)
 iptables -N syn-flood
 iptables -A INPUT -p tcp --syn -j syn-flood
@@ -133,12 +156,15 @@ iptables -A syn-flood -m limit --limit 1/s --limit-burst 4 -j RETURN
 iptables -A syn-flood -m limit --limit 1/s --limit-burst 4 -j LOG --log-prefix "SYN-flood: "
 iptables -A syn-flood -j DROP
 
+echo "Ochrona przed atakiem SYN-FLOOD"
 # Syn-flood protection
 iptables -A INPUT -p tcp --syn -m limit --limit 1/s -j ACCEPT
 
+echo "Ochrona przed atakiem FURTIVE PORT SCANNER"
 # Furtive port scanner
 iptables -A INPUT -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -j ACCEPT
 
+echo "Blokowanie wybranych stron"
 # Blokowanie www
 #iptables -A OUTPUT -p tcp -m string --string "onet.pl" --algo kmp -j DROP
 
