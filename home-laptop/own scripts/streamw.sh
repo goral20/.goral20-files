afa4003acd69f4fba951120773a5c80606e89dfc
goral20 <goral20@gmail.com>
2013-08-16 20:45:34 +0200

    script to watch wonziu stream

---
+++ home-laptop/own scripts/streamw.sh
@@ -0,0 +1,62 @@
+ #!/bin/bash
+showMenu () {
+         echo "1) Ogladaj stream Wonzia w jakości 240p z gamecreads"
+         echo "2) Ogladaj stream Wonzia w jakości 380p z gamecreads"
+         echo "3) Ogladaj stream Wonzia w jakości 480p z gamecreads "
+         echo "4) Ogladaj stream Wonzia w jakości 720p z gamecreads"
+         echo "5) Ogladaj stream Wonzia w jakości 240p z twitch"
+         echo "6) Wyjście"
+         echo -n "Wybierz jedną z opcji od 1..4 do 6 !! Opcja: "
+}
+
+while [ 1 ]
+        do
+        showMenu
+        read CHOICE
+        case "$CHOICE" in
+                "1")
+                        echo -e
+                        echo "Wybrałeś: Ogladaj stream Wonzia w jakości 240p z gamecreads"
+                          for (( i = 1 ; i <= 10; i++ )) 
+                          do
+                          livestreamer -p mplayer http://www.dailymotion.com/embed/video/xzlc83 240p
+                          done
+                        ;;
+                "2")    echo -e
+                        echo "Wybrałeś: Ogladaj stream Wonzia w jakości 380p z gamecreads"
+                          x=1;
+                          while [ $x -le 10 ]; do
+                          livestreamer -p mplayer http://www.dailymotion.com/embed/video/xzlc83 380p
+                          done
+                        ;;
+                "3")    echo -e
+                        echo "Wybrałeś: Ogladaj stream Wonzia w jakości 480p z gamecreads"
+                          n=1;
+                          while [ $n -le 5 ]
+                          do
+                          livestreamer -p mplayer http://www.dailymotion.com/embed/video/xzlc83 480p
+                          n=$[n + 1]
+                          done
+                          continue
+                        ;;
+                "4")    echo -e
+                        echo "Wybrałeś: Ogladaj stream Wonzia w jakości i720p z gamecreads"
+                          x=1;
+                          while [ $x -le 10 ]; do
+                          livestreamer -p mplayer http://www.dailymotion.com/embed/video/xzlc83 720p
+                          done
+                        ;;
+                "5")    echo -e
+                        echo "Wybrałeś: Ogladaj stream Wonzia w jakości 240p z twitch"
+                          x=1;
+                          while [ $x -le 10 ]; do
+                          livestreamer -p mplayer http://www.dailymotion.com/embed/video/xzlc83 240p
+                          done
+                        ;;
+                "6")
+                        echo "Wybrałeś: Wyjście"
+                        exit
+                        ;;
+        esac
+done
+
