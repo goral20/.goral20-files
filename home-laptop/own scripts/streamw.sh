8906a1015d2dba95fe381ae010fde66f1e33d5b2
goral20 <goral20@gmail.com>
2013-08-20 23:21:29 +0200

    script to watch wonzay stream from gamecreads and twitch

--- home-laptop/own scripts/streamw.sh
+++ home-laptop/own scripts/streamw.sh
@@ -1,15 +1,26 @@
  #!/bin/bash
+        echo "Podaj liczbę powtórzeń i wybierz stream z podanych jakości"
+        echo "Wpisz liczbę prób: "
+        echo "Wpisz 0 aby wyjść"
+        read prob
+        if [[ $prob -gt 0 ]] ; then
+          continue
+        else
+          exit
+        fi
 showMenu () {
-         echo "Wybierz jedna z 4 opcji i podaj liczbę powtórzeń"
-         echo "1) Ogladaj stream Wonzia w jakości 240p z gamecreads"
-         echo "2) Ogladaj stream Wonzia w jakości 380p z gamecreads"
-         echo "3) Ogladaj stream Wonzia w jakości 480p z gamecreads"
-         echo "4) Ogladaj stream Wonzia w jakości 720p z gamecreads"
-         echo "5) Wyjście"
-         echo "Wpisz liczbę prób: "
-         echo "Wpisz 0 aby wyjść"
-                  read prob
-                  echo -e
+        echo -e
+        echo "01) Ogladaj stream Wonzia w jakości 240p z gamecreads"
+        echo "02) Ogladaj stream Wonzia w jakości 380p z gamecreads"
+        echo "03) Ogladaj stream Wonzia w jakości 480p z gamecreads"
+        echo "04) Ogladaj stream Wonzia w jakości 720p z gamecreads"
+        echo "05) Ogladaj stream Wonzia w jakości 240p z twitch"
+        echo "06) Ogladaj stream Wonzia w jakości 360p z twitch"
+        echo "07) Ogladaj stream Wonzia w jakości 480p z twitch"
+        echo "08) Ogladaj stream Wonzia w jakości 720p z twitch"
+        echo "09) Ogladaj stream Wonzia w jakości 1080p z twitch"
+        echo "Q) Wyjście"
+
          echo -n "Opcja streamu: "
 }
 
@@ -40,7 +51,7 @@ while [ 1 ]
                      done
                      continue
                     ;;
-                "3")   
+                "3")
                      echo -e
                      echo "Wybrałeś: Ogladaj stream Wonzia w jakości 480p z gamecreads"
                      n=1;
@@ -61,8 +72,63 @@ while [ 1 ]
                       n=$[n + 1]
                      done
                      continue
-                    ;;
+                     ;;
+                "5")
+                     echo -e
+                     echo "Wybrałeś: Ogladaj stream Wonzia w jakości 240p z twitch"
+                     n=1;
+                     while [ $n -eq "$prob" ]
+                      do
+                      livestreamer -p mplayer http://www.twitch.tv/wonziu 240p
+                      n=$[n + 1]
+                     done
+                     continue
+                     ;;
                 "6")
+                     echo -e
+                     echo "Wybrałeś: Ogladaj stream Wonzia w jakości 360p z twitch"
+                     n=1;
+                     while [ $n -eq "$prob" ]
+                      do
+                      livestreamer -p mplayer http://www.twitch.tv/wonziu 360p
+                      n=$[n + 1]
+                     done
+                     continue
+                     ;;
+                "7")
+                     echo -e
+                     echo "Wybrałeś: Ogladaj stream Wonzia w jakości 480p z twitch"
+                     n=1;
+                     while [ $n -eq "$prob" ]
+                      do
+                      livestreamer -p mplayer http://www.twitch.tv/wonziu 480p
+                      n=$[n + 1]
+                     done
+                     continue
+                     ;;
+                "8")
+                     echo -e
+                     echo "Wybrałeś: Ogladaj stream Wonzia w jakości 720p z twitch"
+                     n=1;
+                     while [ $n -eq "$prob" ]
+                      do
+                      livestreamer -p mplayer http://www.twitch.tv/wonziu 720p
+                      n=$[n + 1]
+                     done
+                     continue
+                    ;;
+                "9")
+                     echo -e
+                     echo "Wybrałeś: Ogladaj stream Wonzia w jakości 1080p z twitch"
+                     n=1;
+                     while [ $n -eq "$prob" ]
+                      do
+                      livestreamer -p mplayer http://www.twitch.tv/wonziu 1080p
+                      n=$[n + 1]
+                     done
+                     continue
+                    ;;
+                "Q")
                         echo -e
                         echo "Wybrałeś: Wyjście"
                         exit
