61227c278e0cda5480068b44cdfa6afcd5f9b0d9
goral20 <goral20@gmail.com>
2013-09-06 12:57:03 +0200

    py Jaki system?

--- home-laptop/own scripts/python/Jaki system ?.py
+++ home-laptop/own scripts/python/Jaki system ?.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # wersja v1.0.1
 import sys
-print ('Korzystam z systemu:' )
-print (sys.platform)
+import platform
+import socket
+print('Korzystam z systemu: ' + sys.platform)
+print('Posiadam jądro w wersji: ' + platform.release())
+print('Mój hostname to: ' + socket.gethostname())
