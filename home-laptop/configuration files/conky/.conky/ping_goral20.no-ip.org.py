4c708c8c43221373ef4b50715bff49b3e0579209
goral20 <goral20@gmail.com>
2013-09-12 23:22:57 +0200

    ping script for conky

---
+++ home-laptop/configuration files/conky/.conky/ping_goral20.no-ip.org.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import os
+hostname = "sebas87.dyndns.org" #example
+#response = os.system("ping -c 1 " + hostname)
+response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
+if response == 0:
+    print(hostname, 'is up!')
+else:
+    print(hostname, 'is down!')
