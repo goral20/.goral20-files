e3f534e5834b510f3bc362a5d2cfa32de7628c25
goral20 <goral20@gmail.com>
2013-09-13 07:50:56 +0200

    ping script for conky v1.0

--- home-laptop/configuration files/conky/.conky/ping_goral20.no-ip.org.py
+++ home-laptop/configuration files/conky/.conky/ping_goral20.no-ip.org.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-hostname = "sebas87.dyndns.org" #example
+hostname = "goral20.no-ip.org" #example
 #response = os.system("ping -c 1 " + hostname)
 response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
 if response == 0:
