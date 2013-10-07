a08bd7269a7f6590c7a211141275478b33da8b10
goral20 <goral20@gmail.com>
2013-10-07 14:28:45 +0200

    py szkielet skryptu

--- scripts/python/szkielet skryptu.py
+++ scripts/python/szkielet skryptu.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import platform
+if platform.python_version() < "3.0.0": 
+    print("ERROR: Python 3.0 or greater is required for this to run. Sorry")
+    sys.exit()
 polecenie
