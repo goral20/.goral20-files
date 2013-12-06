0f1e83a15ca71488b372045179e799bb4fb936bc
goral20 <goral20@gmail.com>
2013-12-06 23:34:52 +0100

    iSkrypt do oglądania streamów z Gamecreads oraz Twitch v1.02 - poprawiono numer programu

--- scripts/python/Stream.py
+++ scripts/python/Stream.py
@@ -8,7 +8,7 @@ if platform.python_version() < "3.0.0":
     sys.exit()
 
 # Skrypt umożliwiający oglądanie streamów z Gamcreads oraz Twich
-# Program w wersji v1.01
+# Program w wersji v1.02
 # Autor: goral20
 # https://github.com/goral20
 print("")
