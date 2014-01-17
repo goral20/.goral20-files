696731517b55a9523f2a1e43732d1f5d4edb8e70
goral20 <goral20@gmail.com>
2014-01-17 23:59:43 +0100

    Skrypt do oglądania streamów z Gamecreads oraz Twitch v1.03 - dodano streamy od hayuto

--- scripts/python/Stream.py
+++ scripts/python/Stream.py
@@ -8,14 +8,15 @@ if platform.python_version() < "3.0.0":
     sys.exit()
 
 # Skrypt umożliwiający oglądanie streamów z Gamcreads oraz Twich
-# Program w wersji v1.02
+# Program w wersji v1.03
 # Autor: goral20
 # https://github.com/goral20
 print("")
-print("Wybierz platformę")
-print("1. Gamecreads")
-print("2. Twitch")
-print("3. HitBox")
+print("Wybierz streamera/platformę")
+print("1. Wonziu Gamecreads")
+print("2. Wonziu Twitch")
+print("3. Wonziu HitBox")
+print("4. Hayuto")
 platforma = input("Wybieram: ")
 if platforma == '1':
     # print("gamecreads")
@@ -25,9 +26,9 @@ elif platforma == '2':
     platforma = 'http://www.twitch.tv/wonziu'
 elif platforma == '3':
     platforma = 'http://hitbox.tv/#!/embed/vonzay'
-else:
-    print("syf")
-    platforma = 'syf'
+elif platforma == '4':
+    platforma = 'rtmp://37.59.202.234:13335/live/hayuto'
+    subprocess.call(["mpv", "rtmp://37.59.202.234:13335/live/hayuto"])
 # print(platforma)
 
 print("")
@@ -36,6 +37,7 @@ print("1. 240p")
 print("2. 380p")
 print("3. 480p")
 print("4. 720p")
+print("5. bez wyboru jakości")
 jakosc = input("Wybieram: ")
 if jakosc == '1':
     # print("240p")
@@ -49,6 +51,9 @@ elif jakosc == '3':
 elif jakosc == '4':
     # print("720p")
     jakosc = '720p'
+elif jakosc == '5':
+    # print("720p")
+    jakosc = ''
 else:
     print("QUIT")
 # print(jakosc)
