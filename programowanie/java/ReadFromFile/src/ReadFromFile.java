3fececdd4239d43815a7248748c5b63e0ae16508
goral20 <goral20@gmail.com>
2015-01-13 21:16:59 +0100

    java - program do wyświetlnia zawartości pliku test - wersja 1

--- programowanie/java/ReadFromFile/src/ReadFromFile.java
+++ programowanie/java/ReadFromFile/src/ReadFromFile.java
@@ -1,32 +1,31 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
  
+ 
 public class ReadFromFile {
  
-	public static void main(String[] args) {
+        public static void main(String[] args) {
+                String everything = "";
+                try(BufferedReader br = new BufferedReader(new FileReader("/home/sebastian/Dokumenty/Programowanie/ReadFromFile/src/test"))) {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
  
-		BufferedReader br = null;
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                everything = sb.toString();
+            } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                }
+                System.out.println(everything);
+        }
  
-		try {
- 
-			String sCurrentLine;
- 
-			br = new BufferedReader(new FileReader("test"));
- 
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
-			}
- 
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (br != null)br.close();
-			} catch (IOException ex) {
-				ex.printStackTrace();
-			}
-		}
- 
-	}
 }
